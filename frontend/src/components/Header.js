e017b174ec8e5e0d1e195369c30b0342bbc65ab1
imimran <engr.aih@gmail.com>
2020-11-02 17:20:55 +0600

    implement slidebar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import { Route } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { LinkContainer } from "react-router-bootstrap";
 import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 import { logout } from "../store/actions/userActions";
 
+
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import { Link } from "react-router-dom";
+import { SidebarData } from "./SidebarData";
+import "./Navbar.css";
+import { IconContext } from "react-icons";
+
 const Header = () => {
+   const [sidebar, setSidebar] = useState(false);
+
+   const showSidebar = () => setSidebar(!sidebar);
   const dispatch = useDispatch();
 
   const userLogin = useSelector((state) => state.userLogin);
@@ -19,6 +30,34 @@ const Header = () => {
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
+          <IconContext.Provider value={{ color: "#fff" }}>
+          
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Link to="#" className="menu-bars">
+                <FaIcons.FaBars onClick={showSidebar} />
+              </Link>
+            </Navbar.Collapse>
+            <Nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+              <ul className="nav-menu-items" onClick={showSidebar}>
+                <li className="navbar-toggle">
+                  <Link to="#" className="menu-bars">
+                    <AiIcons.AiOutlineClose />
+                  </Link>
+                </li>
+                {SidebarData.map((item, index) => {
+                  return (
+                    <li key={index} className={item.cName}>
+                      <Link to={item.path}>
+                        {item.icon}
+                        <span>{item.title}</span>
+                      </Link>
+                    </li>
+                  );
+                })}
+              </ul>
+            </Nav>
+          </IconContext.Provider>
+
           <LinkContainer to="/">
             <Navbar.Brand>Employee Management</Navbar.Brand>
           </LinkContainer>
---
+++ frontend/src/components/SidebarData.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import * as IoIcons from 'react-icons/io';
+import * as BsIcons from "react-icons/bs";
+
+export const SidebarData = [
+  {
+    title: "Home",
+    path: "/",
+    icon: <AiIcons.AiFillHome />,
+    cName: "nav-text",
+  },
+  {
+    title: "Company",
+    path: "/organizations",
+    icon: <BsIcons.BsTable />,
+    cName: "nav-text",
+  },
+
+  {
+    title: "Employee",
+    path: "/employees",
+    icon: <IoIcons.IoMdPeople />,
+    cName: "nav-text",
+  },
+  {
+    title: "Announce",
+    path: "/announcements",
+    icon: <FaIcons.FaEnvelopeOpenText />,
+    cName: "nav-text",
+  },
+
+];
