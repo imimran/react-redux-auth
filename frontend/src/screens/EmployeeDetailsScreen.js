b2b10c9040959229a9e7e37d1b07317480eb8466
imimran <engr.aih@gmail.com>
2020-11-17 17:10:28 +0600

    display employee image

--- frontend/src/screens/EmployeeDetailsScreen.js
+++ frontend/src/screens/EmployeeDetailsScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Card, Container } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 import FormContainer from "../components/FormContainer";
 import { useDispatch, useSelector } from "react-redux";
 import { employeeDetails } from "../store/actions/employeeActions";
@@ -29,8 +29,8 @@ const EmployeeDetailsScreen = ({match}) => {
         <Card style={{ width: "18rem" }}>
           {employee && (
             <Card.Body>
-             
               <Card.Title>Employee details</Card.Title>
+              <Card.Img variant="top" src={`http://localhost:4000/uploads/${employee.image}`} />
               <Card.Text>ID: {employee.id} </Card.Text>
               <Card.Text>Name: {employee.name} </Card.Text>
 
